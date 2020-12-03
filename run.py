bc3e90e00a21f2fe39c7cb02b33d09e2393f1284
Giulia Berto <gberto@fbk.eu>
2020-12-03 16:39:47 +0100

    copy entire qa dir to secondary folder

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 import os
 import json
-from shutil import copyfile
+from shutil import copytree
 
 results = {"errors": [], "warnings": []}
 
@@ -12,12 +12,5 @@ sec_dir = 'secondary'
 if not os.listdir(src_dir):
     results['errors'].append("Output directory is empty")
 
-if not os.path.exists(sec_dir):
-    os.mkdir(sec_dir)
-
 qa_dir = src_dir + '/qa'
-fig_left = '/qa_seg_multiatlas_corr_nogray_left_qa.png'
-fig_right = '/qa_seg_multiatlas_corr_nogray_right_qa.png'
-
-copyfile(qa_dir + fig_left, sec_dir + fig_left)
-copyfile(qa_dir + fig_right, sec_dir + fig_right)
+copytree(qa_dir, sec_dir)
